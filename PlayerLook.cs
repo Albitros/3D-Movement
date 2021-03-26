2bf118cf3aa47f13c95e6b515387ba75a798aef1
Albitros <77668252+Albitros@users.noreply.github.com>
2021-03-26 17:30:51 +0100

    Add files via upload

---
+++ PlayerLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Transform player;
+    public Transform head;
+
+
+    public float mouseSensiticity;
+    private float x = 0f;
+    private float y = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        transform.position = head.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        x += -Input.GetAxis("Mouse Y") * mouseSensiticity;
+        y += Input.GetAxis("Mouse X") * mouseSensiticity;
+        //Clamping
+        x = Mathf.Clamp(x, -90, 90);
+        //rotating
+        transform.localRotation = Quaternion.Euler(x, 0, 0);
+        player.transform.localRotation = Quaternion.Euler(0, y, 0);
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public LayerMask ground;
+
+    public bool grounded;
+
+
+    public Transform groundPos;
+    public float moveSpeed;
+    public float jumpForce;
+    public float radious;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //input
+        float x = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        float y = Input.GetAxisRaw("Vertical") * moveSpeed;
+        //moving player
+        Vector3 pos = transform.right * x + transform.forward * y;
+        Vector3 newPos = new Vector3(pos.x, rb.velocity.y, pos.z);
+
+        rb.velocity = newPos;
+        //jumping
+        grounded = Physics.CheckSphere(new Vector3(groundPos.transform.position.x, groundPos.transform.position.y, groundPos.transform.position.z), radious, ground);
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+        //sprinting
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = 12;
+        }
+        else if (!Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = 6;
+        }
+
+
+
+
+    }
+}
